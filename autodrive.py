6df4bee9eddb13eee6459c61e46abd4fef61914d
robocar <>
2026-01-20 12:14:20 +0100

    [Fix]

--- autodrive.py
+++ autodrive.py
@@ -3,7 +3,7 @@ from autodrive.autodriveState import AutoDriveState
 
 if __name__ == "__main__":
     try:
-        manager = Manager(AutoDriveState)
+        manager = Manager(AutoDriveState())
         manager.loop()
     except KeyboardInterrupt:
         manager.safe_stop()
