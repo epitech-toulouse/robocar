9bcf50be492875934d7e1ab4b46582d6be0a9564
Theo Darques <theo.darques@epitech.eu>
2026-01-20 11:03:37 +0100

    [ADD] better border getter

--- autodrive/lidarPrinter.py
+++ autodrive/lidarPrinter.py
@@ -1,14 +1,27 @@
-#print the nearest detected object distance
+#store the nearest point detected on each degree to known the car boundary
 
 from LidarParser import LidarParser
 import time
 
 lidar = LidarParser()
-while True:
+car_border_dist = [0.0] * 360
+running = True
+
+while running:
+    #check if user input command exit to stop program
+    if input("Press enter to stop or 'q' to quit: ") == 'q':
+        running = False
+    #check if user input command to get car border
+    if input("Press 'a' to get car border: ") == 'a':
+        print(f"Car border: {car_border_dist}")
     points = lidar.get_points()
     if not points:
         time.sleep(0.05)
         continue
-    min_dist = min([p['distance'] for p in points])
-    print(f"Nearest object: {min_dist:.2f}m")
+    for p in points:
+        angle = p['angle']
+        distance = p['distance']
+        if distance < car_border_dist[angle]:
+            car_border_dist[angle] = distance
+    print(f"Car border: {car_border_dist}")
     time.sleep(0.05)
