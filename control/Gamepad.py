d2c282a54e8959734d30c369635d2213a8b97776
Mani Gillier <mani.gillier@epitech.eu>
2026-01-26 18:32:58 +0100

    fixup! [Feat]

--- control/Gamepad.py
+++ control/Gamepad.py
@@ -73,11 +73,9 @@ class Gamepad:
                 if value.code == 0:
                     self.axis["steering"] = abs_event.event.value / 32767
                 elif value.code == 5:
-                    self.axis["forward"] = abs_event.event.value / 255
-                    * self.max_speed_ratio
+                    self.axis["forward"] = abs_event.event.value / 255 * self.max_speed_ratio
                 elif value.code == 2:
-                    self.axis["backward"] = abs_event.event.value / 255
-                    * self.max_speed_ratio / 2
+                    self.axis["backward"] = abs_event.event.value / 255 * self.max_speed_ratio / 2
             # If button
             elif value.code == evdev.ecodes.BTN_A:
                 self.buttons["A"] = True
