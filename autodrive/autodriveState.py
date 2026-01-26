40472372f19d8305737911eb0d9dc04571c09ce8
pintat-gil <diego.pintat-gil@gmail.com>
2026-01-26 16:58:10 +0100

    fix backward speed

--- autodrive/autodriveState.py
+++ autodrive/autodriveState.py
@@ -228,6 +228,6 @@ class AutoDriveState(State):
     def handle_reverse(self, motor: Motor, largescans):
         """Gère la marche arrière"""
         print(f"🔄 MARCHE ARRIÈRE ({self.reverse_timer} cycles restants)")
-
-        if (self.reverse_timer < 3):
+        if (self.reverse_timer < 10):
+            print("🔙 FIN DE MARCHE ARRIÈRE dans 10 cycle restet steering")
             motor.set_steering_objective(0.0);
