7a76cb11ea365496f36e01db4d76362107a6dfd8
Mani Gillier <mani.gillier@epitech.eu>
2026-03-31 17:32:57 +0200

    [Feat] Add driving algorithm interface

---
+++ esp/src/vescController/main/api/driving_algorithm_interface.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2026
+** robocar
+** File description:
+** driving algo api
+*/
+
+#ifndef DRIVING_ALGO_API_HPP
+#define DRIVING_ALGO_API_HPP
+
+struct DrivingAlgorithmOutput {
+    // [-1.0;+1.0]
+    float target_speed;
+    // [0.0;1.0]
+    float target_steering;
+};
+
+static DrivingAlgorithmOutput const DEFAULT_DRIVING_ALGORITHM_OUTPUT = {
+    // Stop car
+    .target_speed = 0.0,
+    // Center direction
+    .target_steering = 0.5
+};
+
+class DrivingAlgorithmApi {
+public:
+    virtual ~DrivingAlgorithmApi() = default;
+
+    // Test if necessary inputs are present (but not if correct)
+    virtual bool available(void) = 0;
+    
+    // Computation for a single tick
+    // Returns false if no output can be computed (invalid inputs)
+    // => output set to DEFAULT_DRIVING_ALGORITHM_OUTPUT
+    // Returns true if output can be computer
+    virtual bool compute(DrivingAlgorithmOutput &output) = 0;
+};
+
+#endif /* DRIVING_ALGO_API_HPP */
